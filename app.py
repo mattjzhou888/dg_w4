6f74266e934ff76a8415a8f1172f9b59fab0a6d7
Matthew Zhou <mattjzhou@lawn-143-215-109-225.lawn.gatech.edu>
2023-04-30 07:21:12 -0400

    Finished Flask Deployment

---
+++ app.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import pickle
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+model = pickle.load(open("model.pickle", "rb"))
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    X = np.array([[int(x) for x in request.form.values()]])
+    pred = model.predict(X)
+    out = round(pred[0], 2)
+
+    return render_template("index.html", prediction_text="Predicted Salary: {}".format(out))
+
+app.run(port=5000)
